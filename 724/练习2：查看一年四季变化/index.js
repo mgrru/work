1bab82434509f6cd7540616903dffcdf6c278fc5
mgrru <ru@fufu.moe>
2024-07-24 20:17:51 +0800

    724作业

---
+++ 724/练习2：查看一年四季变化/index.js
@@ -0,0 +1,22 @@
+const link_a = document.getElementsByTagName("a")
+
+const link_to = (url) => {
+    location.assign(url)
+}
+
+for (let i = 0; i < link_a.length; i++) {
+    let innerHTML = link_a.item(i).innerHTML
+    if (innerHTML.includes("春")) {
+        link_a.item(i).onclick = () => link_to("spring.html")
+    } else if (innerHTML.includes("夏")) {
+        link_a.item(i).onclick = () => link_to("summer.html")
+    } else if (innerHTML.includes("秋")) {
+        link_a.item(i).onclick = () => link_to("autumn.html")
+    } else if (innerHTML.includes("冬")) {
+        link_a.item(i).onclick = () => link_to("winter.html")
+    } else if (innerHTML === "前进") {
+        link_a.item(i).onclick = () => window.history.forward()
+    } else if (innerHTML === "后退") {
+        link_a.item(i).onclick = () => window.history.back()
+    }
+}
