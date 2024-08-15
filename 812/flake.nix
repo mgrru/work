ac6acab897dccc73d01d138c974e4a8addac4901
ru <ru@fufu.moe>
2024-08-15 22:07:59 +0800

    .

--- 812/flake.nix
+++ 812/flake.nix
@@ -1,10 +1,17 @@
 {
   description = "A Nix-flake-based Node.js development environment";
   nixConfig = {
+    trusted-users = [ "ru" ];
     substituters = [
       "https://mirrors.ustc.edu.cn/nix-channels/store"
       "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
       "https://mirror.sjtu.edu.cn/nix-channels/store"
+      "https://nix-community.cachix.org"
+      "https://cache.nixos.org"
+    ];
+    trusted-public-keys = [
+      # nix community's cache server public key
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
   };
 
